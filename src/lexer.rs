e01bc1d604ded5ec683672b6b6a40985b030e6b3
Valery Piashchynski <piashchynski_valery@hotmail.com>
2019-09-22 15:18:23 +0300

    fix null char error when EOF, add tests

--- src/lexer.rs
+++ src/lexer.rs
@@ -26,7 +26,7 @@ impl Lexer {
     ///
     pub fn read_char(&mut self) {
         if self.read_position >= self.input.len() {
-            self.ch = '0';
+            self.ch = '\0';
         } else {
             let (_, chr) = self.input.char_indices().nth(self.read_position).unwrap();
             self.ch = chr;
@@ -48,42 +48,68 @@ impl Lexer {
 
         match self.ch {
             '=' => {
-                token = Token::new_token(ASSIGN.parse::<String>().unwrap(), self.ch);
+                token = Token::new_token(
+                    ASSIGN.parse::<String>().unwrap(),
+                    self.ch
+                );
             }
             ';' => {
-                token = Token::new_token(SEMICOLON.parse::<String>().unwrap(), self.ch);
+                token = Token::new_token(
+                    SEMICOLON.parse::<String>().unwrap(),
+                    self.ch
+                );
             }
             '(' => {
-                token = Token::new_token(LPAREN.parse::<String>().unwrap(), self.ch);
+                token = Token::new_token(
+                    LPAREN.parse::<String>().unwrap(),
+                    self.ch
+                );
             }
             ')' => {
-                token = Token::new_token(RPAREN.parse::<String>().unwrap(), self.ch);
+                token = Token::new_token(
+                    RPAREN.parse::<String>().unwrap(),
+                    self.ch
+                );
             }
             ',' => {
-                token = Token::new_token(COMMA.parse::<String>().unwrap(), self.ch);
+                token = Token::new_token(
+                    COMMA.parse::<String>().unwrap(),
+                    self.ch
+                );
             }
             '+' => {
-                token = Token::new_token(PLUS.parse::<String>().unwrap(), self.ch);
+                token = Token::new_token(
+                    PLUS.parse::<String>().unwrap(),
+                    self.ch
+                );
             }
             '{' => {
-                token = Token::new_token(LBRACE.parse::<String>().unwrap(), self.ch);
+                token = Token::new_token(
+                    LBRACE.parse::<String>().unwrap(),
+                    self.ch
+                );
             }
             '}' => {
-                token = Token::new_token(RBRACE.parse::<String>().unwrap(), self.ch);
+                token = Token::new_token(
+                    RBRACE.parse::<String>().unwrap(),
+                    self.ch);
             }
-            '0' => {
-                token = Token::new_token(EOF.parse::<String>().unwrap(), '0');
+            '\0' => {
+                token = Token::new_token(
+                    EOF.parse::<String>().unwrap(),
+                    '\0'
+                );
             }
             _ => {
                 if self.ch.is_ascii_alphabetic() {
-                    let ident = self.read_identifier();
-                    let tok_type = Token::lookup_ident(ident.clone());
-                    token = Token::new_token_string(ident.clone(), tok_type.to_string().clone());
+                    let literal = self.read_identifier();
+                    let tok_type = Token::lookup_ident(literal.clone());
+                    token = Token::new_token_string(tok_type.to_string().clone(), literal.clone());
                     return token;
                 } else if self.ch.is_ascii_digit() {
-                    let ident = INT.parse::<String>().unwrap();
-                    let tok_type = self.read_number();
-                    token = Token::new_token_string(ident.clone(), tok_type.clone());
+                    let tok_type = INT.parse::<String>().unwrap();
+                    let literal = self.read_number();
+                    token = Token::new_token_string(tok_type.clone(), literal.clone());
                     return token;
                 } else {
                     token = Token::new_token(ILLEGAL.parse::<String>().unwrap(), self.ch)
@@ -137,9 +163,7 @@ impl Lexer {
             '\r' => {
                 self.read_char();
             }
-            _ => {
-
-            }
+            _ => {}
         }
     }
 }
@@ -163,51 +187,49 @@ let result = add(five, ten);".to_string();
 
         let mut l = Lexer::new(input);
 
-        let tokens:[Token; 37] = [
+        let tokens: [Token; 37] = [
             Token::new_token_string(LET.to_string(), "let".to_string()),
-            Token::new_token_string("five".to_string(), IDENT.to_string()),
-            Token::new_token_string("=".to_string(), ASSIGN.to_string()),
-            Token::new_token_string("5".to_string(), INT.to_string()),
-            Token::new_token_string(";".to_string(), SEMICOLON.to_string()),
-            Token::new_token_string("let".to_string(), LET.to_string()),
-            Token::new_token_string("ten".to_string(), IDENT.to_string()),
-            Token::new_token_string("=".to_string(), ASSIGN.to_string()),
-            Token::new_token_string("10".to_string(), INT.to_string()),
-            Token::new_token_string(";".to_string(), SEMICOLON.to_string()),
-            Token::new_token_string("let".to_string(), LET.to_string()),
-            Token::new_token_string("add".to_string(), IDENT.to_string()),
-            Token::new_token_string("=".to_string(), ASSIGN.to_string()),
-            Token::new_token_string("fn".to_string(), FUNCTION.to_string()),
-            Token::new_token_string("(".to_string(), LPAREN.to_string()),
-            Token::new_token_string("x".to_string(), IDENT.to_string()),
-            Token::new_token_string(",".to_string(), COMMA.to_string()),
-            Token::new_token_string("y".to_string(), IDENT.to_string()),
-            Token::new_token_string(")".to_string(), RPAREN.to_string()),
-            Token::new_token_string("{".to_string(), LBRACE.to_string()),
-            Token::new_token_string("x".to_string(), IDENT.to_string()),
-            Token::new_token_string("+".to_string(), PLUS.to_string()),
-            Token::new_token_string("y".to_string(), IDENT.to_string()),
-            Token::new_token_string(";".to_string(), SEMICOLON.to_string()),
-            Token::new_token_string("}".to_string(), RBRACE.to_string()),
-            Token::new_token_string(";".to_string(), SEMICOLON.to_string()),
-            Token::new_token_string("let".to_string(), LET.to_string()),
-            Token::new_token_string("result".to_string(), IDENT.to_string()),
-            Token::new_token_string("=".to_string(), ASSIGN.to_string()),
-            Token::new_token_string("add".to_string(), IDENT.to_string()),
-            Token::new_token_string("(".to_string(), LPAREN.to_string()),
-            Token::new_token_string("five".to_string(), IDENT.to_string()),
-            Token::new_token_string(",".to_string(), COMMA.to_string()),
-            Token::new_token_string("ten".to_string(), IDENT.to_string()),
-            Token::new_token_string(")".to_string(), RPAREN.to_string()),
-            Token::new_token_string(";".to_string(), SEMICOLON.to_string()),
-            Token::new_token_string("".to_string(), EOF.to_string()),
-
+            Token::new_token_string(IDENT.to_string(), "five".to_string()),
+            Token::new_token_string(ASSIGN.to_string(), "=".to_string()),
+            Token::new_token_string(INT.to_string(), "5".to_string()),
+            Token::new_token_string(SEMICOLON.to_string(), ";".to_string()),
+            Token::new_token_string(LET.to_string(), "let".to_string()),
+            Token::new_token_string(IDENT.to_string(), "ten".to_string()),
+            Token::new_token_string(ASSIGN.to_string(), "=".to_string()),
+            Token::new_token_string(INT.to_string(), "10".to_string()),
+            Token::new_token_string(SEMICOLON.to_string(), ";".to_string()),
+            Token::new_token_string(LET.to_string(), "let".to_string()),
+            Token::new_token_string(IDENT.to_string(), "add".to_string()),
+            Token::new_token_string(ASSIGN.to_string(), "=".to_string()),
+            Token::new_token_string(FUNCTION.to_string(), "fn".to_string()),
+            Token::new_token_string(LPAREN.to_string(), "(".to_string()),
+            Token::new_token_string(IDENT.to_string(), "x".to_string()),
+            Token::new_token_string(COMMA.to_string(), ",".to_string()),
+            Token::new_token_string(IDENT.to_string(), "y".to_string()),
+            Token::new_token_string(RPAREN.to_string(), ")".to_string()),
+            Token::new_token_string(LBRACE.to_string(), "{".to_string()),
+            Token::new_token_string(IDENT.to_string(), "x".to_string()),
+            Token::new_token_string(PLUS.to_string(), "+".to_string()),
+            Token::new_token_string(IDENT.to_string(), "y".to_string()),
+            Token::new_token_string(SEMICOLON.to_string(), ";".to_string()),
+            Token::new_token_string(RBRACE.to_string(), "}".to_string()),
+            Token::new_token_string(SEMICOLON.to_string(), ";".to_string()),
+            Token::new_token_string(LET.to_string(), "let".to_string()),
+            Token::new_token_string(IDENT.to_string(), "result".to_string()),
+            Token::new_token_string(ASSIGN.to_string(), "=".to_string()),
+            Token::new_token_string(IDENT.to_string(), "add".to_string()),
+            Token::new_token_string(LPAREN.to_string(), "(".to_string()),
+            Token::new_token_string(IDENT.to_string(), "five".to_string()),
+            Token::new_token_string(COMMA.to_string(), ",".to_string()),
+            Token::new_token_string(IDENT.to_string(), "ten".to_string()),
+            Token::new_token_string(RPAREN.to_string(), ")".to_string()),
+            Token::new_token_string(SEMICOLON.to_string(), ";".to_string()),
+            Token::new_token_string(EOF.to_string(), "\0".to_string()),
         ];
 
         for t in tokens.iter() {
             // implemented partialeq for struct
             assert_eq!(*t, l.next_token());
         }
-
     }
 }
