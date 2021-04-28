4fb48e60d12a1dd230bef6b6ba78d886cbd0a146
Valery Piashchynski <piashchynski.valery@gmail.com>
2021-04-28 22:02:40 +0300

    Don’t mess with Voodoo

--- src/token.rs
+++ src/token.rs
@@ -10,16 +10,29 @@ pub const IDENT: &str = "IDENT";
 pub const INT: &str = "INT";
 
 // operators
-pub const ASSIGN: &str = "=";
-pub const PLUS: &str = "+";
-pub const MINUS: &str = "-";
-pub const BANG: &str = "!";
-pub const ASTERISK: &str = "*";
-pub const SLASH: &str = "/";
-pub const LT: &str = "<";
-pub const GT: &str = ">";
-pub const EQ: &str = "==";
-pub const NOT_EQ: &str = "!=";
+pub enum Operators<'a> {
+    ASSIGN(&'a str),
+    PLUG(&'a str),
+    MINUS(&'a str),
+    BANG(&'a str),
+    ASTERISK(&'a str),
+    SLASH,
+    LT,
+    GT,
+    EQ,
+    NOT_EQ,
+}
+
+// pub const ASSIGN: &str = "=";
+// pub const PLUS: &str = "+";
+// pub const MINUS: &str = "-";
+// pub const BANG: &str = "!";
+// pub const ASTERISK: &str = "*";
+// pub const SLASH: &str = "/";
+// pub const LT: &str = "<";
+// pub const GT: &str = ">";
+// pub const EQ: &str = "==";
+// pub const NOT_EQ: &str = "!=";
 
 // delimiters
 pub const COMMA: &str = "COMMA";
@@ -40,21 +53,21 @@ pub const ELSE: &str = "ELSE";
 pub const RETURN: &str = "RETURN";
 
 // TODO maybe not String but &str ???
-type TokenType = String;
+type TokenType<'a> = &'a str;
 
-lazy_static! {
-static ref HASHMAP: Mutex<HashMap<String, TokenType>> = {
-    let mut m = HashMap::new();
-    m.insert("let".to_string(), LET.to_string());
-    m.insert("fn".to_string(), FUNCTION.to_string());
-    m.insert("true".to_string(), TRUE.to_string());
-    m.insert("false".to_string(), FALSE.to_string());
-    m.insert("if".to_string(), IF.to_string());
-    m.insert("else".to_string(), ELSE.to_string());
-    m.insert("return".to_string(), RETURN.to_string());
-    Mutex::new(m)
-    };
-}
+// lazy_static! {
+// static ref HASHMAP<'a> : Mutex<HashMap<String, TokenType<'a>>> = {
+//     let mut m = HashMap::new();
+//     m.insert("let".to_string(), LET);
+//     m.insert("fn".to_string(), FUNCTION);
+//     m.insert("true".to_string(), TRUE);
+//     m.insert("false".to_string(), FALSE);
+//     m.insert("if".to_string(), IF);
+//     m.insert("else".to_string(), ELSE);
+//     m.insert("return".to_string(), RETURN);
+//     Mutex::new(m)
+//     };
+// }
 
 #[derive(Debug, Clone)]
 pub struct Token {
